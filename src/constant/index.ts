329d219dcad39f27d61d8c43b8923d01f433a9d0
Denmark Dano <kramdano@gmail.com>
2025-11-04 06:09:49 +0800

    Use HTTPS for OMDB API

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,7 +3,7 @@ import { CustomGenre } from "src/types/Genre";
 export const API_ENDPOINT_URL = import.meta.env.VITE_APP_API_ENDPOINT_URL || "https://api.themoviedb.org/3";
 export const TMDB_V3_API_KEY = import.meta.env.VITE_APP_TMDB_V3_API_KEY?.trim() || "";
 export const OMDB_API_KEY = import.meta.env.VITE_APP_OMDB_API_KEY?.trim() || "87a82b55";
-export const OMDB_API_URL = "http://www.omdbapi.com";
+export const OMDB_API_URL = "https://www.omdbapi.com";
 
 export const MAIN_PATH = {
   root: "",
